9a5461b9671d05af0b0fe04952e42031d1a363bd
huongnguyenduc <38987479+huongnguyenduc@users.noreply.github.com>
2022-10-25 23:37:48 +0700

    Fix UI

---
+++ MoneyTracker/MoneyTracker/ViewModels/AddAccountViewModel.cs
@@ -0,0 +1,65 @@
+﻿using MoneyTracker.Views;
+using System.Globalization;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MoneyTracker.ViewModels
+{
+    public class AddAccountViewModel : BaseViewModel
+    {
+        private string _name;
+        private float _initialBalance;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public float InitialBalance
+        {
+            get { return _initialBalance; }
+            set
+            {
+                _initialBalance = value;
+                OnPropertyChanged(nameof(InitialBalance));
+            }
+        }
+
+        public string CurrencySymbol
+        {
+            // TODO: Help user self-change Currency
+            get { return NumberFormatInfo.CurrentInfo.CurrencySymbol; }
+        }
+
+        public ICommand AddWalletCommand => new Command(AddWallet);
+        async void AddWallet()
+        {
+            if (Name == null || Name == "" || InitialBalance == null)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert!", "Fill all fields", "Ok");
+                return;
+            }
+
+            if (await Services.DatabaseConnection.VerifyIfAccExist(Name))
+            {
+                await Services.DatabaseConnection.AddAccount(
+                    new Models.Account
+                    {
+                        Name = Name,
+                        Balance = InitialBalance
+                    });
+                await Application.Current.MainPage.DisplayAlert("Success!", "Wallet Added", "Ok");
+                await Application.Current.MainPage.Navigation.PushAsync(new MainTabbedPage());
+
+            }
+            else
+                await Application.Current.MainPage.DisplayAlert("Alert!", "Wallet Exist, try another Name", "Ok");
+            Name = "";
+            InitialBalance = 0;
+        }
+    }
+}
--- MoneyTracker/MoneyTracker/Views/AddAccountPage.xaml.cs
+++ MoneyTracker/MoneyTracker/Views/AddAccountPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MoneyTracker.Views
@@ -8,48 +6,9 @@ namespace MoneyTracker.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddAccountPage : ContentPage
     {
-        public string Name { get; set; }
-        public float InitialBalance { get; set; }
-
         public AddAccountPage()
         {
             InitializeComponent();
-            BindingContext = this;
-        }
-
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            if (Name == null || Name == "" || InitialBalance == null)
-            {
-                await DisplayAlert("Alert!", "Fill all fields", "Ok");
-                return;
-            }
-
-            if (await Services.DatabaseConnection.VerifyIfAccExist(Name))
-            {
-                await Services.DatabaseConnection.AddAccount(
-                    new Models.Account
-                    {
-                        Name = Name,
-                        Balance = InitialBalance
-                    });
-                await DisplayAlert("Success!", "Wallet Added", "Ok");
-                await Navigation.PushAsync(new MainTabbedPage());
-
-            }
-            else
-                await DisplayAlert("Alert!", "Wallet Exist, try another Name", "Ok");
-
-            accountName.Text = "";
-            accountBalance.Text = "";
-
-            Name = "";
-            InitialBalance = 0;
-        }
-
-        private async void Button_Clicked_1(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new MainTabbedPage());
         }
     }
 }
